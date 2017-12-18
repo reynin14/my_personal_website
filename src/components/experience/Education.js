918da6f329c2815aee5aef8d777d4af23a8677e3
github username <github email address>
2017-12-17 16:27:59 -0800

    added in education container and section

---
+++ src/components/experience/Education.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Education extends React.Component{
+  render(){
+    return(
+      <div>
+        <h2>Educations</h2>
+        <h4><b>Flatiron School - New York, NY - <i>2017</i></b></h4><br></br>
+        <p>Full Stack Web Development, Ruby on Rails and JavaScript immersive program</p>
+        <h4><b>University of California, Irvine - Irvine, CA - <i>2013</i></b></h4><br></br>
+        <p>Bachelor of Arts degree: Business Economics</p>
+      </div>
+    )
+  }
+}
+
+export default Education
