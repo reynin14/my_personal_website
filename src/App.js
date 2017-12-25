83b62f6c91a82e6a662c36cb66bb881c16e87762
github username <github email address>
2017-12-24 17:06:55 -0800

    fixed update bug, and added experience

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import BioContainer from './components/bio/BioContainer'
+import ExperienceContainer from './components/experience/ExperienceContainer'
+
+
 
 class App extends Component {
   render() {
     return (
       <div>
         <BioContainer />
-
+        <ExperienceContainer />
       </div>
     );
   }
--- src/components/experience/ExperienceContainer.js
+++ src/components/experience/ExperienceContainer.js
@@ -4,7 +4,7 @@ class ExperienceContainer extends React.Component{
   render(){
     return(
       <div>
-        <p>In ExperienceContainer</p>
+        <p>In Experience Container</p>
       </div>
     )
   }
