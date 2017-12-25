9dd1f7d0f91fd1c76bbfbb335c154acb96918095
github username <github email address>
2017-12-24 18:11:03 -0800

    UBL Interactive WH added

--- src/components/experience/WorkHistory.js
+++ src/components/experience/WorkHistory.js
@@ -12,6 +12,12 @@ class WorkHistory extends React.Component {
       <p>Designed templates and process flows to accommodate for expedited timelines the clients required</p>
       <p>Trained and mentored 15+ new employees on how to efficiently use the company’s proprietary platform for survey programming</p>
 
+      <h4><b>Product Quality Specialist</b> - UBL Interactive Inc - Irvine, CA - <i>May 2014 - June 2015</i></h4>
+      <p>Collaborated with domestic and international small businesses to develop social media listings on various platforms such as Facebook, Google, and Yellow Pages</p>
+      <p>Boosted companies’ online presence by 50% through creative advertisements</p>
+      <p>Streamlined website quality checking protocols to enhance efficiency and increase client satisfaction</p>
+      <p>Delegated incoming job orders to team members and ensured that the marketing listings were being created according to protocol</p>
+
       </div>
     )
   }
