7ead1ec4b2b80496ccef7c6ae3dbfdce428e3881
github username <github email address>
2017-12-24 18:40:42 -0800

    skeleton information completed

--- src/components/contact/ContactInfo.js
+++ src/components/contact/ContactInfo.js
@@ -5,6 +5,10 @@ class ContactInfo extends React.Component{
     return(
       <div>
         <h2>Contact Information</h2>
+        <h4>Email - </h4>
+        <p>kyonzon14@gmail.com</p>
+        <h4>LinkedIn - </h4>
+        <p>www.linkedin.com/in/kevin-yonzon</p>
       </div>
     )
   }
