ce79c27f208bdd17819bec0d6edcd459f4224529
github username <github email address>
2018-01-07 18:20:44 -0800

    larger and new image added, working on repositioning

--- src/components/backgroundLayers/NameIntroBackground.js
+++ src/components/backgroundLayers/NameIntroBackground.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import kyBridge from '../../images/kyBridge.JPG'
+import kyLeavesActual from '../../images/kyLeavesActual.JPG'
 
 class NameIntroBackground extends React.Component{
   render(){
@@ -9,11 +9,10 @@ class NameIntroBackground extends React.Component{
         Width: '100%',
         height: '100%',
         margin: '0 auto',
-        backgroundImage: `url(${kyBridge})`,
-        backgroundSize: '100% 80%',
+        backgroundImage: `url(${kyLeavesActual})`,
+        backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
-        backgroundPostion: 'center',
-        backgroundOrigin: 'content-box'
+        objectPosition: '100% 150%'
         }}
       >
       </div>
